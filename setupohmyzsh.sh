6b52f262a9be5185d739f9ed7c5206124547de3f
Chemputer <46601333+chemputer@users.noreply.github.com>
2021-06-26 19:32:48 -0400

    added install ZSH, of all things.

--- setupohmyzsh.sh
+++ setupohmyzsh.sh
@@ -5,7 +5,8 @@ sudo apt install python3 -y
 sudo apt install powerline-fonts -y
 sudo apt install fonts-powerline -y
 sudo apt install python3-pip -y
-sudo apt install python3-wheel
+sudo apt install python3-wheel -
+sudo apt install zsh -y
 sudo pip3 install autoenv pygments thefuck
 export ZSH="$HOME/.oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
